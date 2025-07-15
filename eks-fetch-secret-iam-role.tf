99b4ba99eb39e951f6d0b8b7b317309ce6b12381
mjth <mjth830@gmail.com>
2025-07-15 22:17:08 +0300

    adding iam role with the policy and rbac role inside the cluster for the user that will create the sealed-secret for the controller

---
+++ eks-fetch-secret-iam-role.tf
@@ -0,0 +1,103 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "sealed-secret" {
+  name = "${var.env}-${var.project}-fetch-sealed-secret"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/awscli"
+          ]
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name       = "sealed-secret"
+    Managed_By = "Terraform"
+    Project    = var.project
+  }
+}
+
+resource "aws_iam_policy" "fetch-sealed-secret" {
+  name = "${var.env}-${var.project}-fetch-sealed-secrets"
+
+  policy = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ssm:GetParameter",
+            "Resource": "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:parameter/mprofile-*"
+        },
+        {
+        Effect   = "Allow"
+        Action   = "eks:ListClusters"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "eks:DescribeCluster"
+        Resource = "arn:aws:eks:${var.region}:${data.aws_caller_identity.current.account_id}:cluster/${var.eks_cluster_name}"
+      }
+    ]
+}
+EOT
+}
+
+resource "aws_iam_role_policy_attachment" "sealed-secret" {
+  role       = aws_iam_role.sealed-secret.name
+  policy_arn = aws_iam_policy.fetch-sealed-secret.arn
+}
+
+resource "kubernetes_role_v1" "secret-role" {
+  metadata {
+    name = "sealed-secrets"
+    labels = {
+      usage = "creating-secret-for-sealed-secret-controller"
+    }
+  }
+
+  rule {
+    api_groups     = [""]
+    resources      = ["secrets"]
+    resource_names = ["mprofile-sealed-secret"]
+    verbs          = ["*"]
+  }
+}
+
+resource "kubernetes_role_binding_v1" "sealed-secret-binding" {
+  metadata {
+    name      = "sealed-secrets-binding"
+    namespace = "kube-system"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "sealed-secrets"
+  }
+
+  subject {
+    kind      = "User"
+    name      = "sealed-secret-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "aws_eks_access_entry" "sealed-secret-admin" {
+  cluster_name      = var.eks_cluster_name
+  principal_arn     = aws_iam_role.sealed-secret.arn
+
+  depends_on = [ kubernetes_role_binding_v1.sealed-secret-binding ]
+}
